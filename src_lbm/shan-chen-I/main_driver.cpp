141d8b2f71b8a68407473fe6d459570aeef05b03
root <root@LAPTOP-KSQ1DS4A.localdomain>
2025-06-01 05:57:34 -0400

    Calculate Structure Factor

--- src_lbm/shan-chen-I/main_driver.cpp
+++ src_lbm/shan-chen-I/main_driver.cpp
@@ -13,49 +13,16 @@ using namespace amrex;
 #include "AMReX_FileIO.H"
 
 // default grid parameters
-IntVect domain_size(6);
-IntVect max_box_size(3);
+IntVect domain_size(32);
+IntVect max_box_size(16);
 
 // default time stepping parameters
 int nsteps = 5;
 
-// default output parameters
-int plot_SF = 0;
-
-inline void ReadInput() {
-  ParmParse pp;
-
-  /* grid parameters */
-  pp.query("nx", domain_size[0]);
-  domain_size[2] = domain_size[1] = domain_size[0]; // default to cubic box
-  pp.query("ny", domain_size[1]);
-  pp.query("nz", domain_size[2]);
-
-  pp.query("max_grid_size_x", max_box_size[0]);
-  max_box_size[2] = max_box_size[1] = max_box_size[0]; // default to same maxSize in all directions
-  pp.query("max_grid_size_y", max_box_size[1]);
-  pp.query("max_grid_size_z", max_box_size[2]);
-
-  // initial condition
-  init_cond = "droplet";
-  pp.query("init_cond", init_cond);
-  pp.query("init_frac", init_frac);
-
-  // thermodynamic parameters
-  pp.query("G", G);
-  pp.query("temperature", temperature);
-
-  /* time stepping and output parameters */
-  pp.query("nsteps", nsteps);
-  pp.query("plot_int", plot_int);
-  pp.query("plot_SF", plot_SF);
-
-}
-
 inline void WriteOutput(int step,
       const Geometry& geom,
 			const MultiFab& hydrovs,
-      StructFact& structFact) {
+      StructFact& structFact, int plot_SF=0) {
   // set up variable names for output
   const int zero_avg = 1;
   const Vector<std::string> var_names = hydrovars_names(hydrovs.nComp());
@@ -73,7 +40,23 @@ int main(int argc, char* argv[]) {
   // read input parameters
   //ReadInput();
 
-  plot_int = 1;
+  // [true] for a: kBT=0; b: kBT>0 && switching on noise for the FIRST time;
+  // [false] only if hope to continue from chkpoint in which noise>0;
+  bool continueFromNonFluct = true;//false; 
+
+  // Total number of steps to run; MUST be integer multiples of [plot_int];
+  int nsteps = 500000;//100000;
+  int plot_int = 1000;//10;//1000; // output configurations every [plot_int] steps;
+  int print_int = 100;      // print out info every [print_int] steps;
+  /*specifying time window for calculating the equilibrium state solution;
+    usually be set as multiples of [plot_int], from step [last_step_index-t_window] to [last_step_index];
+    i.e., [numOfFrames-1]*[plot_int], in which last_step_index is also multiples of [plot_int] */
+  const int t_window = 10*plot_int; 
+  int out_noise_step = plot_int;    // output noise terms every [out_noise_step] steps;
+
+  // [plot_SF_window] is the time window for calculating the structure factor;
+  int plot_SF_window = 100000; // not affected by [plot_int]; out freq controlled by [out_SF_step]
+  int out_SF_step = 50;
 
   // set up Box and Geomtry
   RealBox real_box({0.,0.,0.},{1.,1.,1.});
@@ -107,6 +90,8 @@ int main(int argc, char* argv[]) {
 
   // INITIALIZE
   LBM_init(geom, fold, gold, hydrovs);
+  Print() << "\tLB step " << 0 << std::endl;
+  //PrintMultiFabComp(fold, 0);
   if (plot_int > 0) WriteOutput(0, geom, hydrovs, structFact);
   //Print() << "LB initialized lattice " << domain <<"\n" << ba << dm << std::endl;
 
@@ -117,19 +102,21 @@ int main(int argc, char* argv[]) {
   // copy the reference state
   ParallelCopy(refstate, hydrovs, 0, 0, 2, IntVect(nghost), IntVect(nghost));
   
-  PrintMultiFabComp(fold, 3, 0);
-
   // TIMESTEP
+  int SF_start = nsteps - plot_SF_window; int plot_SF = 1;
   for (int step=1; step <= nsteps; ++step) {
+    //Print() << "\tLB step " << step << std::endl;
     LBM_timestep(geom, fold, gold, fnew, gnew, hydrovs, refstate);
-    PrintMultiFabComp(fold, 3, 0);
-    if (plot_SF > 0) structFact.FortStructure(hydrovs);
-    if (plot_int > 0 && step%plot_int ==0) {
-      
-      WriteOutput(step, geom, hydrovs, structFact);
+    if (step>=SF_start && step%out_SF_step == 0 && plot_SF > 0) structFact.FortStructure(hydrovs, 0);
+    //PrintMultiFabComp(fold, 0);
+    if (plot_int > 0 && step%plot_int ==0 && step<nsteps) {
       Print() << "\t**************************************\t" << std::endl;
       Print() << "\tLB step " << step << std::endl;
       Print() << "\t**************************************\t" << std::endl;
+      WriteOutput(step, geom, hydrovs, structFact, 0);
+    }
+    if(step == nsteps){
+      WriteOutput(step, geom, hydrovs, structFact, plot_SF);
     }
   }
 
